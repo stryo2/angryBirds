35c212a15be7b9652b7a289ab5d68a12844b27d4
stryo2 <preu2727@gmail.com>
2025-06-08 12:48:36 +0530

    first commit

---
+++ box.js
@@ -0,0 +1,12 @@
+class Box{
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    show(){
+        fill(255);
+        rect(this.x, this.y, this.width, this.height);
+    }
+}
