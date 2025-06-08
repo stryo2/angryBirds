e412dbb08f2cdbcc6020864834090fe8a157b3fb
stryo2 <preu2727@gmail.com>
2025-06-08 13:35:07 +0530

    added a bird object

--- sketch.js
+++ sketch.js
@@ -1,10 +1,16 @@
 let ground;
+let box;
+let bird;
 
 function setup() {
     createCanvas(600, 400);
     ground = new Box(0, height - 20, width, 20);
+    box = new Box(450, 300, 50, 75);
+    bird = new Bird(100,320,20);
 }
 function draw(){
     background(0);
     ground.show();
+    box.show();
+    bird.show();
 }
